4ca07d8203a14f8f97df0debe4b9df00e1b97421
etctera <yilonglei790@gmail.com>
2023-08-11 23:50:32 +0800

    init: 项目初始化

---
+++ .eslintrc.cjs
@@ -0,0 +1,32 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true
+  },
+  extends: [
+    "prettier",
+    "plugin:prettier/recommended",
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:react/jsx-runtime",
+    "plugin:@typescript-eslint/recommended"
+  ],
+  overrides: [],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module"
+  },
+  plugins: ["prettier", "react", "@typescript-eslint"],
+  rules: {
+    "prettier/prettier": "error",
+    "@typescript-eslint/no-non-null-assertion": "off",
+    "@typescript-eslint/no-explicit-any": "off"
+  },
+  settings: {
+    react: {
+      version: "detect"
+    }
+  }
+}
