aeabbb2720c3244d1f22d6ee12a3c24fdea45b48
etctera <yilonglei790@gmail.com>
2023-08-20 23:47:27 +0800

    feat: commit workflow

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -24,7 +24,8 @@ module.exports = {
     "@typescript-eslint/no-non-null-assertion": "off",
     "@typescript-eslint/no-explicit-any": "off",
     "@typescript-eslint/ban-ts-comment": "off",
-    "react/prop-types": "off"
+    "react/prop-types": "off",
+    "react/display-name": "off"
   },
   settings: {
     react: {
